e4724c0940e31c094fb03c5ffe2cafa162fe2609
suntosh <suntosh@gmail.com>
2026-08-19 10:06:58 -0500

    removed legacy stuff

--- legacy/original-Exceptions.py
+++
@@ -1,7 +0,0 @@
-class ECE608_GraphException(Exception):
-    """Base class for exceptions in Graph traversal or Graph ."""
-
-class ECE608_GraphError(ECE608_GraphException):
-    """Exception for a serious error in NetworkX"""
-
-
